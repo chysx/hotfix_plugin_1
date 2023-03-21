517894e24276dc6439576eaf869f0f227ef6847c
zhangguopeng <976990321@qq.com>
2023-03-21 23:43:24 +0800

    message

--- android/src/main/kotlin/com/zhang/hotfix_plugin_1/patch/download/AssetsPatchDownloader.kt
+++ android/src/main/kotlin/com/zhang/hotfix_plugin_1/patch/download/AssetsPatchDownloader.kt
@@ -1,6 +1,7 @@
 package com.zhang.hotfix_plugin_1.patch.download
 
 import android.content.Context
+import android.util.Log
 import com.zhang.hotfix_plugin_1.FileUtil
 import com.zhang.hotfix_plugin_1.UnFile
 import com.zhang.hotfix_plugin_1.http.apiService
@@ -10,20 +11,26 @@ import java.io.*
 
 class AssetsPatchDownloader: IPatchDownloader {
     override fun download(context: Context, patchPath: String): Boolean{
+        Log.e("AssetsPatchDownloader", "***** patchPath = " + patchPath)
         val result = kotlin.runCatching {
             val response =
                 apiService.downloadAssets(patchPath).execute()
             if(response.isSuccessful) {
+                Log.e("AssetsPatchDownloader", "***** asset资源下载成功")
                 val body = response.body()
                 if(body != null){
+                    Log.e("AssetsPatchDownloader", "***** asset资源body 有值")
                     FileUtil.copy(
                         body.byteStream(),
                         FileOutputStream(Constant.localPatchPath(context) + Constant.assetZip)
                     )
+                    Log.e("AssetsPatchDownloader", "***** asset资源文件写入成功：" + Constant.localPatchPath(context) + Constant.assetZip)
                     UnFile.upZipFile(
                         Constant.localPatchPath(context) + Constant.assetZip,
                         Constant.localPatchPath(context)
                     )
+                    Log.e("AssetsPatchDownloader", "***** asset资源文件解压成功：")
+
                     File(Constant.localPatchPath(context) + Constant.assetZip).deleteOnExit()
                 }else {
                     throw Exception("body 为 null")
