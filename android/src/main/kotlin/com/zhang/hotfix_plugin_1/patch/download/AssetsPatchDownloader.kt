62b9930d61e1dfcf13ca705c683e9b9b9a8bc13f
zhangguopeng <976990321@qq.com>
2023-03-22 21:44:19 +0800

    message

--- android/src/main/kotlin/com/zhang/hotfix_plugin_1/patch/download/AssetsPatchDownloader.kt
+++ android/src/main/kotlin/com/zhang/hotfix_plugin_1/patch/download/AssetsPatchDownloader.kt
@@ -10,34 +10,46 @@ import java.io.*
 
 
 class AssetsPatchDownloader: IPatchDownloader {
-    override fun download(context: Context, patchPath: String): Boolean{
-        Log.e("AssetsPatchDownloader", "***** patchPath = " + patchPath)
-        val result = kotlin.runCatching {
-            val response =
-                apiService.downloadAssets(patchPath).execute()
-            if(response.isSuccessful) {
-                Log.e("AssetsPatchDownloader", "***** asset资源下载成功")
-                val body = response.body()
-                if(body != null){
-                    Log.e("AssetsPatchDownloader", "***** asset资源body 有值")
-                    FileUtil.copy(
-                        body.byteStream(),
-                        FileOutputStream(Constant.localPatchPath(context) + Constant.assetZip)
-                    )
-                    Log.e("AssetsPatchDownloader", "***** asset资源文件写入成功：" + Constant.localPatchPath(context) + Constant.assetZip)
-                    UnFile.upZipFile(
-                        Constant.localPatchPath(context) + Constant.assetZip,
-                        Constant.localPatchPath(context)
-                    )
-                    Log.e("AssetsPatchDownloader", "***** asset资源文件解压成功：")
+//    override fun download(context: Context, patchPath: String): Boolean{
+//        Log.e("AssetsPatchDownloader", "***** patchPath = " + patchPath)
+//        val result = kotlin.runCatching {
+//            val response =
+//                apiService.downloadAssets(patchPath).execute()
+//            if(response.isSuccessful) {
+//                Log.e("AssetsPatchDownloader", "***** asset资源下载成功")
+//                val body = response.body()
+//                if(body != null){
+//                    Log.e("AssetsPatchDownloader", "***** asset资源body 有值")
+//                    FileUtil.copy(
+//                        body.byteStream(),
+//                        FileOutputStream(Constant.localPatchPath(context) + Constant.assetZip)
+//                    )
+//                    Log.e("AssetsPatchDownloader", "***** asset资源文件写入成功：" + Constant.localPatchPath(context) + Constant.assetZip)
+//                    UnFile.upZipFile(
+//                        Constant.localPatchPath(context) + Constant.assetZip,
+//                        Constant.localPatchPath(context)
+//                    )
+//                    Log.e("AssetsPatchDownloader", "***** asset资源文件解压成功：")
+//
+//                    File(Constant.localPatchPath(context) + Constant.assetZip).deleteOnExit()
+//                }else {
+//                    throw Exception("body 为 null")
+//                }
+//            }else {
+//                throw Exception("response 响应失败")
+//            }
+//        }
+//        result.exceptionOrNull()?.printStackTrace()
+//        return result.isSuccess
+//    }
 
-                    File(Constant.localPatchPath(context) + Constant.assetZip).deleteOnExit()
-                }else {
-                    throw Exception("body 为 null")
-                }
-            }else {
-                throw Exception("response 响应失败")
-            }
+    override fun download(context: Context, patchPath: String): Boolean{
+        val result = kotlin.runCatching {
+            File(Constant.localExternalPath() + Constant.assetDir).copyRecursively(
+                File(Constant.localPatchPath(context = context) + Constant.assetDir),
+                overwrite = true
+            )
+            Log.e("AssetsPatchDownloader", "***** asset资源copy成功")
         }
         result.exceptionOrNull()?.printStackTrace()
         return result.isSuccess
